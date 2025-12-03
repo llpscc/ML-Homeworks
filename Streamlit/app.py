39c0be44711d26dc93ab1da8c4a3101c8b267999
llpscc <l.pascenco@gmail.com>
2025-12-03 23:10:47 +0200

    Refactor input collection and prediction output
    Updated user input handling and prediction display in the Streamlit app.

--- Streamlit/app.py
+++ Streamlit/app.py
@@ -2,28 +2,38 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.linear_model import Ridge
 
+# Загрузка модели
 with open("streamlit_model.pkl", "rb") as f:
     bundle = pickle.load(f)
 
 pipeline = bundle["pipeline"]
-columns = bundle["columns"]
 inverse_transform = bundle["inverse_transform"]
-mean_values = bundle["mean_values"]
 
+input_features = pipeline.named_steps['preprocessor'].transformers_[0][2] + \
+                 pipeline.named_steps['preprocessor'].transformers_[1][2]
+
+# Интерфейс
 st.title("Предсказание стоимости автомобиля")
-st.markdown("Введите значения признаков:")
+st.markdown("Введите характеристики авто:")
 
 user_input = {}
-for col in columns:
-    user_input[col] = st.number_input(col, value=float(mean_values[col]))
+
+for col in input_features:
+    user_input[col] = st.text_input(col) if col.isalpha() else st.number_input(col)
 
 input_df = pd.DataFrame([user_input])
-input_df = input_df[columns] 
-input_df = input_df.fillna(mean_values)
 
-log_price = pipeline.predict(input_df)
-predicted_price = inverse_transform(log_price)[0]
+# Предсказание
+try:
+    log_price = pipeline.predict(input_df)
+    predicted_price = inverse_transform(log_price)[0]
 
-st.subheader("Предсказанная цена автомобиля:")
-st.write(f"**{predicted_price:,.0f} ₽**")
+    st.subheader("Предсказанная цена:")
+    st.write(f"**{predicted_price:,.0f} ₽**")
+except Exception as e:
+    st.error(f"Ошибка: {e}")
